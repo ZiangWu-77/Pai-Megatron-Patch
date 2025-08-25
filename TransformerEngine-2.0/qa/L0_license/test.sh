2ff5f33e499366534567e68ac56a293ae70469e0
Ziang Wu <2587359106@qq.com>
2025-08-26 01:47:06 +0800

    delete TransformerEngine-2.0

--- TransformerEngine-2.0/qa/L0_license/test.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-#
-# See LICENSE for license information.
-
-set -e
-
-: "${TE_PATH:=/opt/transformerengine}"
-
-python $TE_PATH/qa/L0_license/copyright_checker.py $TE_PATH
